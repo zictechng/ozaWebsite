84a3a82c96d54d5ae07163bc25333324b6c8b90f
Zictech <zictechng@gmail.com>
2026-09-13 15:30:20 +0100

    feat: local files from github to pc and running in localhost

--- src/component/client.js
+++ src/component/client.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-//export default axios.create({baseURL: 'http://192.168.1.83:3500'});
+export default axios.create({baseURL: 'http://192.168.1.73:3500'});
 
 //export default axios.create({baseURL: 'https://crane-cuff-links.cyclic.cloud'})
 
-export default axios.create({baseURL: 'https://ozabackendapi.ozaapp.com'})
+//export default axios.create({baseURL: 'https://ozabackendapi.ozaapp.com'})
 
 //export default axios.create({baseURL: 'https://ozabackendapi.ozaapp.com'})
