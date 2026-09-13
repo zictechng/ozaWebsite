e73629c0fdf42b53513b5b5878cd8edb64f94710
Zictech <zictechng@gmail.com>
2026-09-13 15:31:22 +0100

    localhost port updated

--- src/component/client.js
+++ src/component/client.js
@@ -6,4 +6,3 @@ export default axios.create({baseURL: 'http://192.168.1.73:3500'});
 
 //export default axios.create({baseURL: 'https://ozabackendapi.ozaapp.com'})
 
-//export default axios.create({baseURL: 'https://ozabackendapi.ozaapp.com'})
